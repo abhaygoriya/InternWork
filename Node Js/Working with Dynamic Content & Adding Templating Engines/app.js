f75ad745d2fb7de86bdafd4b50167b38e019272b
Abhay Goriya <abhay.goriya@prominentpixel.com>
2024-11-28 19:16:07 +0530

    Add pug and haldlebars

---
+++ Node Js/Working with Dynamic Content & Adding Templating Engines/app.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+const hbs = expressHbs.create({
+  defaultLayout: "main-layout",
+  layoutsDir: path.join(__dirname, "views", "layouts"),
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+// app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes.routes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+
+app.listen(3000);
