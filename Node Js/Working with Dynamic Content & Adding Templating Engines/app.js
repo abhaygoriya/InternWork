255a6396c92eefaffe5178771c3df0536947e50d
Abhay Goriya <abhay.goriya@prominentpixel.com>
2024-11-29 11:15:44 +0530

    Add ejs

--- Node Js/Working with Dynamic Content & Adding Templating Engines/app.js
+++ Node Js/Working with Dynamic Content & Adding Templating Engines/app.js
@@ -6,14 +6,17 @@ const expressHbs = require("express-handlebars");
 
 const app = express();
 
-const hbs = expressHbs.create({
-  defaultLayout: "main-layout",
-  layoutsDir: path.join(__dirname, "views", "layouts"),
-});
+//Handlebars
+// const hbs = expressHbs.create({
+//   defaultLayout: "main-layout",
+//   layoutsDir: path.join(__dirname, "views", "layouts"),
+// });
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 // app.set("view engine", "pug");
+
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
@@ -27,7 +30,7 @@ app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
 });
 
 app.listen(3000);
