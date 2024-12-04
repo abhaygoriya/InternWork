a330cc132e247517ef8b4a0dfed2461b7b88d5ea
Abhay Goriya <abhay.goriya@prominentpixel.com>
2024-12-04 17:40:26 +0530

    Remove comment and order model

--- Node Js/Working with NoSQL & Using MongoDB/controllers/shop.js
+++ Node Js/Working with NoSQL & Using MongoDB/controllers/shop.js
@@ -12,29 +12,10 @@ exports.getProducts = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
-  // Product.fetchAll()
-  //   .then(([rows, fieldData]) => {
-  //     res.render("shop/product-list", {
-  //       prods: rows,
-  //       pageTitle: "All Products",
-  //       path: "/products",
-  //     });
-  //   })
-  //   .catch((err) => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-
-  // Product.findAll({ where: { id: prodId } });
-  // .then((products) => {
-  //   res.render("shop/product-detail", {
-  //     product: products[0],
-  //     pageTitle: products[0].title,
-  //     path: "/products",
-  //   });
-  // })
-  // .catch((err) => console.log(err));
 
   Product.findByID(prodId)
     .then((product) => {
@@ -45,16 +26,6 @@ exports.getProduct = (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
-
-  // Product.findById(prodId)
-  //   .then(([product]) => {
-  //     res.render("shop/product-detail", {
-  //       product: product[0],
-  //       pageTitle: product.title,
-  //       path: "/products",
-  //     });
-  //   })
-  //   .catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
@@ -69,15 +40,6 @@ exports.getIndex = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
-  // Product.fetchAll()
-  //   .then(([rows, fieldData]) => {
-  //     res.render("shop/index", {
-  //       prods: rows,
-  //       pageTitle: "Shop",
-  //       path: "/",
-  //     });
-  //   })
-  //   .catch((err) => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
@@ -103,39 +65,6 @@ exports.postCart = (req, res, next) => {
       res.redirect("/cart");
     })
     .catch((err) => console.log(err));
-  // let fetchedCart;
-  // let newQuantity = 1;
-
-  // req.user
-  //   .getCart()
-  //   .then((cart) => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then((products) => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then((product) => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: {
-  //         quantity: newQuantity,
-  //       },
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect("/cart");
-  //   })
-  //   .catch((err) => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
